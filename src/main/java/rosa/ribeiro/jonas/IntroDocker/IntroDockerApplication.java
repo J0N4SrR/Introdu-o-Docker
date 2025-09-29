0f80a7cca099fc96fed7612f96a4e36918cfb0c5
Jonas Ribeiro <krnsjrr@gmail.com.br>
2025-09-29 08:42:11 -0300

    first commit

---
+++ src/main/java/rosa/ribeiro/jonas/IntroDocker/IntroDockerApplication.java
@@ -0,0 +1,13 @@
+package rosa.ribeiro.jonas.IntroDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rosa/ribeiro/jonas/IntroDocker/IntroDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package rosa.ribeiro.jonas.IntroDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
