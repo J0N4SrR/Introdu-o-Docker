0c8a0aa5322d948a7e6c128aff27105e7075c6b4
Jonas Ribeiro <krnsjrr@gmail.com.br>
2025-09-29 08:42:42 -0300

    Add class Greating

---
+++ src/main/java/rosa/ribeiro/jonas/IntroDocker/HelloDockerController.java
@@ -0,0 +1,14 @@
+package rosa.ribeiro.jonas.IntroDocker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloDockerController {
+    @GetMapping
+    public String Greeting(){
+        return "Hello, Docker";
+    }
+}
